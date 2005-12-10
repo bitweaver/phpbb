904d1563be2c24427fe83efa8dd5d968a3c88279
Max Kremmel <xing@synapse.plus.com>
2005-12-10 10:53:27 +0000

    use PKG_DIR as key for registerAppMenu

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -3,7 +3,7 @@ global $gBitSystem;
 
 $gBitSystem->registerPackage( 'phpbb', dirname( __FILE__ ).'/' );
 if( $gBitSystem->isPackageActive( 'phpbb' ) ) {
-	$gBitSystem->registerAppMenu( 'phpbb', 'Forums', PHPBB_PKG_URL.'index.php', 'bitpackage:phpbb/menu_phpbb.tpl' );
+	$gBitSystem->registerAppMenu( PHPBB_PKG_DIR, 'Forums', PHPBB_PKG_URL.'index.php', 'bitpackage:phpbb/menu_phpbb.tpl' );
 	if( file_exists( PHPBB_PKG_PATH.'config.php' ) ) {
 		require_once( PHPBB_PKG_PATH.'config.php' );
 	}
