12274d79e2723dbcc6a8d9a102f99cb897dd5ac0
Max Kremmel <xing@synapse.plus.com>
2006-02-05 21:20:23 +0000

    remove unused parameters in registerPackageInfo

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -3,9 +3,8 @@ global $gBitInstaller;
 $gBitInstaller->registerPackageInfo( PHPBB_PKG_NAME, array(
 	'description' => "phpBB is a high powered, fully scalable, and highly customizable Open Source bulletin board package. phpBB has a user-friendly interface, simple and straightforward administration panel, and helpful FAQ.",
 	'license' => '<a href="http://www.gnu.org/licenses/licenses.html#LGPL">LGPL</a>',
-	'version' => '2.0.15',
+	'version' => '2.0.19',
 	'state' => 'external package',
-	'dependencies' => '',
 	'install' => array(
 		'package' => 'phpbb',
 		'file' => 'install/install.php'
