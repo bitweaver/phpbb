27b88eed007e5b0b83405520d3fb39e368dc7d81
Christian Fowler <spider@viovio.com>
2006-02-18 16:58:06 +0000

    registerPackage changes

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -1,7 +1,11 @@
 <?php
 global $gBitSystem;
 
-$gBitSystem->registerPackage( 'phpbb', dirname( __FILE__ ).'/' );
+$registerHash = array(
+	'package_name' => 'phpbb',
+	'package_path' => dirname( __FILE__ ).'/',
+);
+$gBitSystem->registerPackage( $registerHash );
 if( $gBitSystem->isPackageActive( 'phpbb' ) ) {
 	$gBitSystem->registerAppMenu( PHPBB_PKG_NAME, ucfirst( PHPBB_PKG_DIR ), PHPBB_PKG_URL.'index.php', 'bitpackage:phpbb/menu_phpbb.tpl' );
 	if( file_exists( PHPBB_PKG_PATH.'config.php' ) ) {
