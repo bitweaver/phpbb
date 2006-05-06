dbb87a5b771eaeecb77dd8df2dc8371a0d211c05
Christian Fowler <spider@viovio.com>
2006-05-06 10:51:24 +0000

    revert R2 registerPackage call

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -1,11 +1,7 @@
 <?php
 global $gBitSystem;
 
-$registerHash = array(
-	'package_name' => 'phpbb',
-	'package_path' => dirname( __FILE__ ).'/',
-);
-$gBitSystem->registerPackage( $registerHash );
+$gBitSystem->registerPackage( 'phpbb', dirname( __FILE__ ).'/' );
 
 if( $gBitSystem->isPackageActive( 'phpbb' ) ) {
 	$gBitSystem->registerAppMenu( PHPBB_PKG_NAME, ucfirst( PHPBB_PKG_DIR ), PHPBB_PKG_URL.'index.php', 'bitpackage:phpbb/menu_phpbb.tpl' );
