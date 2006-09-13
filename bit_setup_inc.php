26d296c11de47570eef52ac3222f3c6304135348
Christian Fowler <spider@viovio.com>
2006-09-13 01:47:45 +0000

    revert back to 1.7 which was good to begin with

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -1,16 +1,21 @@
 <?php
 global $gBitSystem;
 
-$gBitSystem->registerPackage( 'phpbb', dirname( __FILE__ ).'/' );
+$registerHash = array(
+	'package_name' => 'phpbb',
+	'package_path' => dirname( __FILE__ ).'/',
+	'homeable' => TRUE,
+);
+$gBitSystem->registerPackage( $registerHash );
 
 if( $gBitSystem->isPackageActive( 'phpbb' ) ) {
 	$menuHash = array(
-		'package_name' => PHPBB_PKG_NAME,
-		'menu_title' => ucfirst( PHPBB_PKG_DIR ),
-		'url_index' => PHPBB_PKG_URL.'index.php',
+		'package_name'  => PHPBB_PKG_NAME,
+		'index_url'     => PHPBB_PKG_URL.'index.php',
 		'menu_template' => 'bitpackage:phpbb/menu_phpbb.tpl',
 	);
 	$gBitSystem->registerAppMenu( $menuHash );
+
 	if( file_exists( PHPBB_PKG_PATH.'config.php' ) ) {
 		require_once( PHPBB_PKG_PATH.'config.php' );
 	}
