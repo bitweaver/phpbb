9144cf898de4c73283a86a2869efd04fd216fa80
Christian Fowler <spider@viovio.com>
2006-09-13 01:35:11 +0000

    oops, revert R2 change made in R1 branch

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -1,21 +1,10 @@
 <?php
 global $gBitSystem;
 
-$registerHash = array(
-	'package_name' => 'phpbb',
-	'package_path' => dirname( __FILE__ ).'/',
-	'homeable' => TRUE,
-);
-$gBitSystem->registerPackage( $registerHash );
+$gBitSystem->registerPackage( 'phpbb', dirname( __FILE__ ).'/' );
 
 if( $gBitSystem->isPackageActive( 'phpbb' ) ) {
-	$menuHash = array(
-		'package_name'  => PHPBB_PKG_NAME,
-		'index_url'     => PHPBB_PKG_URL.'index.php',
-		'menu_template' => 'bitpackage:phpbb/menu_phpbb.tpl',
-	);
-	$gBitSystem->registerAppMenu( $menuHash );
-
+	$gBitSystem->registerAppMenu( PHPBB_PKG_NAME, ucfirst( PHPBB_PKG_DIR ), PHPBB_PKG_URL.'index.php', 'bitpackage:phpbb/menu_phpbb.tpl' );
 	if( file_exists( PHPBB_PKG_PATH.'config.php' ) ) {
 		require_once( PHPBB_PKG_PATH.'config.php' );
 	}
