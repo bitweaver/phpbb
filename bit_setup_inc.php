5aab1a0b0c3424bc313f35d1c19c58022692330c
Christian Fowler <spider@viovio.com>
2006-09-13 01:38:17 +0000

    update registerAppMenu for hashList API change

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -4,7 +4,13 @@ global $gBitSystem;
 $gBitSystem->registerPackage( 'phpbb', dirname( __FILE__ ).'/' );
 
 if( $gBitSystem->isPackageActive( 'phpbb' ) ) {
-	$gBitSystem->registerAppMenu( PHPBB_PKG_NAME, ucfirst( PHPBB_PKG_DIR ), PHPBB_PKG_URL.'index.php', 'bitpackage:phpbb/menu_phpbb.tpl' );
+	$menuHash = array(
+		'package_name' => PHPBB_PKG_NAME,
+		'menu_title' => ucfirst( PHPBB_PKG_DIR ),
+		'url_index' => PHPBB_PKG_URL.'index.php',
+		'menu_template' => 'bitpackage:phpbb/menu_phpbb.tpl',
+	);
+	$gBitSystem->registerAppMenu( $menuHash );
 	if( file_exists( PHPBB_PKG_PATH.'config.php' ) ) {
 		require_once( PHPBB_PKG_PATH.'config.php' );
 	}
